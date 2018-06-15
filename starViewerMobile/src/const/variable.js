bc01098e46ff3a3e7741068bdc37747e7d515215
Quang Lê <ldqntkh@gmail.com>
2018-06-15 09:57:41 +0700

    update api urrl

--- starViewerMobile/src/const/variable.js
+++ starViewerMobile/src/const/variable.js
@@ -1,5 +1,5 @@
 //const API_URL='http://192.168.1.209:18000/api/v1/';
-const API_URL='https://103.235.212.205:9999/api/v1/';
+const API_URL='http://103.235.212.205:9999/api/v1/';
 
 const KEY_USER_LOGIN='user_login';
 const ACCOUNT_LOGIN='account_login';
