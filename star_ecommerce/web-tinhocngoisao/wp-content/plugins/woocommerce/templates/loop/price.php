666a62ad57c9d00192d4b1df1704dc6ad161a97a
unknown <ltphatdev@gmail.com>
2018-08-08 06:47:01 +0700

    make all product section have same height in home page

--- star_ecommerce/web-tinhocngoisao/wp-content/plugins/woocommerce/templates/loop/price.php
+++ star_ecommerce/web-tinhocngoisao/wp-content/plugins/woocommerce/templates/loop/price.php
@@ -25,4 +25,5 @@ global $product;
 
 <?php if ( $price_html = $product->get_price_html() ) : ?>
 	<span class="price"><?php echo $price_html; ?></span>
+	<?php if ( $product->get_sale_price() === '') : ?><br><br><?php endif; ?>
 <?php endif; ?>
