a8008b1cc7210b6a13e8924e2c54008b1021767e
phatleisobar <phat.le@isobar.com>
2018-08-26 10:01:50 +0700

    update product reviews and product description section

--- star_ecommerce/web-tinhocngoisao/wp-content/themes/online-shop/private/javascripts/app.js
+++ star_ecommerce/web-tinhocngoisao/wp-content/themes/online-shop/private/javascripts/app.js
@@ -3,9 +3,11 @@
 var cartpage = require('./cart');
 var accountpage = require('./account');
 var mobile_menu = require('./menu_mobile');
+var productdetailpage = require('./pdp');
 
 $(document).ready(function() {
     cartpage.init();
     accountpage.init();
     mobile_menu.init();
+    productdetailpage.init();
 })
