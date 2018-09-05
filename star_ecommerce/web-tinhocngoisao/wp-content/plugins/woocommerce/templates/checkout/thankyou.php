e661a350f210ca50f9bdf055adc94e1cefd99d02
phatleisobar <41944066+phatleisobar@users.noreply.github.com>
2018-09-05 18:34:05 +0700

    push code version 0.0.1

--- star_ecommerce/web-tinhocngoisao/wp-content/plugins/woocommerce/templates/checkout/thankyou.php
+++ star_ecommerce/web-tinhocngoisao/wp-content/plugins/woocommerce/templates/checkout/thankyou.php
@@ -79,7 +79,77 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<?php do_action( 'woocommerce_thankyou', $order->get_id() ); ?>
 
 	<?php else : ?>
+		<h1 style="text-align: center;">Thank you for your order</h1>
+		<div class="" style="border: 1px solid black;background-color:#f8f8f8;display: inline-flex;flex-direction: row;width:100%;">
+			<div style="width: 50%;flex-direction: column;padding: 10px;border-right: 1px solid black;">
+				<p style="font-weight: bold;text-transform: uppercase;">Summary:</p>
+				<p>Order #: <span>12345678</span></p>
+				<p>Order Date: <span>March 30, 2017</span></p>
+				<p style="matgin-bottom:0;">Order Total: <span>$14.07</span></p>
+			</div>
+			<div style="width: 50%;flex-direction: column;padding: 10px;">
+				<p style="font-weight: bold;text-transform: uppercase;">Shipping address</p>
+				<p>Miss Bean Official</p>
+				<p>123 Sreet Ave</p>
+				<p style="margin-bottom:0;">Boston, MA 02110</p>
+			</div>
+		</div>
 
+		<div style="display: inline-flex;flex-direction: row;width: 100%;background-color:#f8f8f8;margin-top:10px;padding: 20px 10px;">
+			<div class="column-one" style="flex-direction: column;width:85%;">
+				<h3 style="font-weight: bold;text-transform: uppercase;margin:0;">Items shipped</h1>
+			</div>
+			<div class="column-two" style="flex-direction: column;width:5%">
+				<h4 style="margin:0;">QTY</h4>
+			</div>
+			<div style="flex-direction: column;width:10%;text-align:right;">
+				<h5 style="text-transform: uppercase;margin:0;">Price</h5>
+			</div>
+		</div>
+		<div class="product-wrapper" style="display: inline-flex;flex-direction: row;width: 100%;padding: 20px 10px;border-bottom: 2px solid #f8f8f8;">
+			<div class="column-one" style="flex-direction: column;width:20%;">
+				<img src="http://localhost:9999/softwares/starComputer/star_ecommerce/web-tinhocngoisao/wp-content/uploads/2017/12/converse-2483523_640-300x300.jpg"
+					alt="Product Image" width="200" height="200" style="vertical-align:middle;">
+			</div>
+			<div class="column-two" style="flex-direction: column;width: 65%;padding-left:20px;padding-top:5em;">
+				<span>Nylabone Advanced Oral Care Dog Finger Brush, 2-pack</span>
+			</div>
+			<div class="column-three" style="flex-direction: column;width:5%;padding-top:5em;">
+				<span>1</span>
+			</div>
+			<div class="column-four" style="flex-direction: column;width:10%;text-align:right;padding-top:5em;color:red;">
+				<span>$3.59</span>
+			</div>
+		</div>
+		<div class="product-wrapper" style="display: inline-flex;flex-direction: row;width: 100%;padding: 20px 10px;border-bottom: 2px solid #f8f8f8;">
+			<div class="column-one" style="flex-direction: column;width:20%;">
+				<img src="http://localhost:9999/softwares/starComputer/star_ecommerce/web-tinhocngoisao/wp-content/uploads/2017/12/converse-2483523_640-300x300.jpg"
+					alt="Product Image" width="200" height="200" style="vertical-align:middle;">
+			</div>
+			<div class="column-two" style="flex-direction: column;width: 65%;padding-left:20px;padding-top:5em;">
+				<span>Nylabone Advanced Oral Care Dog Finger Brush, 2-pack</span>
+			</div>
+			<div class="column-three" style="flex-direction: column;width:5%;padding-top:5em;">
+				<span>1</span>
+			</div>
+			<div class="column-four" style="flex-direction: column;width:10%;text-align:right;padding-top:5em;color:red;">
+				<span>$3.59</span>
+			</div>
+		</div>
+		<div class="order-summary">
+			<div class="order-summary-column-1">
+				<span>Subtotal (2 items):</span>
+				<span>Flat-Rate Shipping:</span>
+				<span>Estimated Tax:</span>
+				<span>Order Total:</span>
+			</div>
+			<div class="order-summary-column-2">
+				<span>$8.58</span>
+				<span>$4.95</span>
+				<span>$0.54</span>
+				<span>$14.07</span>
+			</div>
+		</div>
 		<p class="woocommerce-notice woocommerce-notice--success woocommerce-thankyou-order-received"><?php echo apply_filters( 'woocommerce_thankyou_order_received_text', __( 'Cám ơn. Vui lòng xem lại hóa đơn của bạn đã thanh toán', 'woocommerce' ), null ); ?></p>
 
 	<?php endif; ?>
