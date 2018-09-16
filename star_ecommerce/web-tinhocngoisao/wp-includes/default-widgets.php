288ae29f66751f7aa64e5eaa5be7ca12e5f2ecd0
Quang Lê <ldqntkh@gmail.com>
2018-09-16 23:19:29 +0700

    register widget marquee

--- star_ecommerce/web-tinhocngoisao/wp-includes/default-widgets.php
+++ star_ecommerce/web-tinhocngoisao/wp-includes/default-widgets.php
@@ -63,3 +63,6 @@ require_once( ABSPATH . WPINC . '/widgets/class-wp-nav-menu-widget.php' );
 
 /** WP_Widget_Custom_HTML class */
 require_once( ABSPATH . WPINC . '/widgets/class-wp-widget-custom-html.php' );
+
+/** WP_Marquee_Widget class */
+require_once( ABSPATH . WPINC . '/widgets/class-wp-marquee-widget.php' );
