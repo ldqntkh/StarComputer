a181a1215118d845306227ca237a28221b4e43fb
Quang Lê <ldqntkh@gmail.com>
2018-09-24 20:53:44 +0700

    init webpack for plugin

---
+++ star_ecommerce/web-tinhocngoisao/wp-content/plugins/woocommerce-sale-date/widget/homepage-widget/assets/private/js/client/reactjs/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import MainComponent from './components/mainComponent';
+
+ReactDOM.render(<MainComponent />, document.getElementById('primetime-price'));
