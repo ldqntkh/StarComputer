b2fea16afac0192de61c9b339c4543046240801a
Quang Lê <ldqntkh@gmail.com>
2018-10-02 14:45:09 +0700

    build reactjs production

--- star_ecommerce/web-tinhocngoisao/wp-content/plugins/woocommerce-sale-date/api/lib/catalogManager.php
+++ star_ecommerce/web-tinhocngoisao/wp-content/plugins/woocommerce-sale-date/api/lib/catalogManager.php
@@ -24,9 +24,6 @@ class CatalogManager {
         $current_hour = $current_hour->format('H');
         while ( $loop->have_posts() ) : $loop->the_post(); 
             global $product;
-            
-            if (!$product->manage_stock) continue;
-
             if ($block_time > $end_block_time) {
                 if ($current_hour >= $block_time && $current_hour <= 24) $end_block_time = 24;
                 else {
@@ -40,24 +37,23 @@ class CatalogManager {
                 if($product->is_on_sale()) {
                     $arrPt['sale_end_time'] = $productMgr->getDiscountTimeRemaining($product->get_id());
                     // xử lý vụ sale cho ngày hôm sau
-                    $arrPt = array(
-                        'id' => $product->id,
-                        'name' => $product->name,
-                        'link' => get_permalink( $product->product_id),
-                        'regular_price' => number_format((float)$product->regular_price, 0, '.', ','),
-                        'sale_price' => number_format((float)$product->sale_price, 0, '.', ','),
-                        'image' => wp_get_attachment_image_src( $product->image_id, 'medium', true )[0],
-                        'average_rating' => $product->average_rating,
-                        'review_count' => $product->review_count
-                    );
-                    //if ($product->manage_stock) {
+                    if ($product->manage_stock && $product->stock_quantity != null) {
+                        $arrPt = array(
+                            'id' => $product->id,
+                            'name' => $product->name,
+                            'link' => get_permalink( $product->product_id),
+                            'regular_price' => number_format((float)$product->regular_price, 0, '.', ','),
+                            'sale_price' => number_format((float)$product->sale_price, 0, '.', ','),
+                            'image' => wp_get_attachment_image_src( $product->image_id, 'medium', true )[0],
+                            'average_rating' => $product->average_rating,
+                            'review_count' => $product->review_count
+                        );
+                    
                         $arrPt['manage_stock'] = true;
                         $arrPt['stock_quantity'] = $product->stock_quantity;
                         $arrPt['stock_status'] = $product->stock_status;
-                    //}
-                    //if ($cat_id > -1) 
-                    
-                    array_push($arrProducts, $arrPt);
+                        array_push($arrProducts, $arrPt);
+                    }
                 }
             } else {
                 $arrPt['sale_end_time'] = $productMgr->getDiscountTimeRemaining($product->get_id());
@@ -72,7 +68,7 @@ class CatalogManager {
                     'average_rating' => $product->average_rating,
                     'review_count' => $product->review_count
                 );
-                if ($product->manage_stock) {
+                if ($product->manage_stock && $product->stock_quantity != null) {
                     $arrPt['manage_stock'] = true;
                     $arrPt['stock_quantity'] = $product->stock_quantity;
                     $arrPt['stock_status'] = $product->stock_status;
@@ -82,7 +78,6 @@ class CatalogManager {
             if (count($arrProducts) >= $post_per_page) break;
         endwhile;
         wp_reset_query();
-        wp_reset_postdata();
         return $arrProducts;
     }
 }
