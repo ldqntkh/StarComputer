8ad86343c2212485d40b1a6b965e80d6d24dc22a
Quang Lê <ldqntkh@gmail.com>
2018-10-18 11:24:32 +0700

    config build sass with webpack

--- star_ecommerce/web-tinhocngoisao/webpack.config.js
+++ star_ecommerce/web-tinhocngoisao/webpack.config.js
@@ -2,9 +2,14 @@
     ./webpack.config.js
 */
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const FixStyleOnlyEntriesPlugin = require("webpack-fix-style-only-entries");
+
+
 module.exports = {
     entry: {
         app: './wp-content/themes/online-shop-child/private/javascripts/app.js',
+        "custom-style" : "./wp-content/themes/online-shop-child/private/scss/style.scss"
     },
     output: {
         path: path.resolve('wp-content/themes/online-shop-child/assets/js'),
@@ -13,14 +18,23 @@ module.exports = {
     mode: 'development',
     module: {
         rules: [{
-                test: /\.css$/,
-                use: [{
-                        loader: "style-loader"
-                    },
-                    {
-                        loader: "css-loader"
-                    }
-                ]
+                test: /.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        {
+                            loader: 'css-loader?url=false',
+                            // options: {
+                            //     modules: true,
+                            //     camelCase: 'dashes',
+                            //     minimize: true
+                            // }
+                        },
+                        {
+                            loader: 'sass-loader'
+                        }
+                    ]
+                })
             },
             {
                 test: /\.js$/,
@@ -30,7 +44,22 @@ module.exports = {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
                 use: "babel-loader"
+            } , 
+            { 
+                test: /\.(png|jpg|gif)$/,
+                exclude: /node_modules/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name : './[path][name].[ext]',
+                        emitFile: false
+                    }
+                }]
             }
         ]
-    }
+    },
+    plugins: [
+        new FixStyleOnlyEntriesPlugin(),
+        new ExtractTextPlugin({ filename: '../../../../../wp-content/themes/online-shop-child/[name].css', disable: false, allChunks: false })
+    ]
 }
