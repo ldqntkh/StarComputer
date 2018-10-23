b9d9bd1a1a630b9cc327a0d1f88aa7740fb2bf6f
phatleisobar <phat.le@isobar.com>
2018-10-23 21:52:46 +0700

    move countdown timer to single template to use

--- star_ecommerce/web-tinhocngoisao/wp-content/themes/online-shop-child/woocommerce/single-product/countdown-time.php
+++ star_ecommerce/web-tinhocngoisao/wp-content/themes/online-shop-child/woocommerce/single-product/countdown-time.php
@@ -2,6 +2,8 @@
     if ( ! defined( 'ABSPATH' ) ) {
         exit; // Exit if accessed directly
     }
+    global $product;
+    $productMgr = new ProductManager();
 ?>
     <div id="woocommerce-product-sale-date" data-sale-time="<?php echo $productMgr->getDiscountTimeRemaining($product->get_id()); ?>">
         <h4><?php echo __('Thời gian khuyến mãi còn lại'); ?></h4>
