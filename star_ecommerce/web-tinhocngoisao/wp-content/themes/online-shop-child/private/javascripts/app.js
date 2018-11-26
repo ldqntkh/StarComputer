0cc4b137b79dc6175197cd7deaede4ce88113113
phatleisobar <phat.le@isobar.com>
2018-11-26 23:01:48 +0700

    push code version 0.0.1

--- star_ecommerce/web-tinhocngoisao/wp-content/themes/online-shop-child/private/javascripts/app.js
+++ star_ecommerce/web-tinhocngoisao/wp-content/themes/online-shop-child/private/javascripts/app.js
@@ -7,6 +7,7 @@ var productdetailpage = require('./pdp');
 var checkoutpage = require('./checkout');
 var custom_filter = require('./custom_filter');
 var showRooms = require('./showroom');
+var buildPC = require('./buildPC');
 
 $(document).ready(function() {
     header_menu.init();
@@ -17,4 +18,5 @@ $(document).ready(function() {
     checkoutpage.init();
     custom_filter.init();
     showRooms.init();
+    buildPC.init();
 })
