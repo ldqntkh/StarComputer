01c801f78c892122ae0962add85079e2e7cb382d
Quang Lê <ldqntkh@gmail.com>
2019-01-12 09:53:32 +0700

    init project StarNet

--- star_ecommerce/web-tinhocngoisao/wp-config.php
+++ star_ecommerce/web-tinhocngoisao/wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'db_tinhocngoisao');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'admin');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'Admin@1234');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
