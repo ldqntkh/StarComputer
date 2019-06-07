0fbacf7084690b73a2ff9a767a4c055a91a52f1d
Le Quang <lequang@MBP-cua-Le.net.fpt>
2019-06-07 20:19:20 +0700

    [Star Ecommerce] remove plugin sale-date

--- star_ecommerce/web-tinhocngoisao/wp-content/plugins/woocommerce-sale-date/widget/homepage-widget/assets/private/js/client/reactjs/stores/indexStore.js
+++
@@ -1,8 +0,0 @@
-import {createStore, combineReducers} from 'redux';
-import { DataPrimeTimeReducer } from '../reducers/dataPrimeTimeReducer';
-
-const PrimeTimeReducer = combineReducers({
-    DataPrimeTimeReducer
-});
-const PrimeTimeStore = createStore(PrimeTimeReducer);
-export default PrimeTimeStore;
