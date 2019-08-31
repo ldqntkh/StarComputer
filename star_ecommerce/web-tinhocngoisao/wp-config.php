deeb8e5c67645cecc5ae8aeead8c45d1748f7fa4
phatleisobar <phat.le@isobar.com>
2019-08-31 23:26:11 +0700

    - Remove accepted parameter in action hook - Use global product instead to get product id

--- star_ecommerce/web-tinhocngoisao/wp-config.php
+++ star_ecommerce/web-tinhocngoisao/wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'db_tinhocngoisao');
 
 /** MySQL database username */
-define('DB_USER', 'admin');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'Admin@1234');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -87,3 +87,5 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+define('WP_CACHE', true);
