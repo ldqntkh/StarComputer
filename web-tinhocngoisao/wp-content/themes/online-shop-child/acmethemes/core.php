8c0343b45e67d992ae45e45e18ffd41f629eb562
Anthony Le <quangld@gearvn.com>
2020-06-20 20:43:50 +0700

    update version

--- web-tinhocngoisao/wp-content/themes/online-shop-child/acmethemes/core.php
+++ web-tinhocngoisao/wp-content/themes/online-shop-child/acmethemes/core.php
@@ -92,7 +92,7 @@ function online_shop_scripts_head() {
     }
     echo '<script type="text/javascript" src="'. get_stylesheet_directory_uri() . '/assets/js/jquery.min.js' .'"></script>';
     wp_enqueue_script('online-shop-custom', get_stylesheet_directory_uri() . '/assets/js/sliderPage.js', array('jquery'), '1.0.0', 1);
-    wp_enqueue_script('online-shop-custom-app', get_stylesheet_directory_uri() . '/assets/js/app.js', array('jquery'), '1.0.4', 1);
+    wp_enqueue_script('online-shop-custom-app', get_stylesheet_directory_uri() . '/assets/js/app.js', array('jquery'), '1.0.5', 1);
 }
 add_action( 'wp_head', 'online_shop_scripts_head' );
 
