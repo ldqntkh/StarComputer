fc3f4c46c97fe35b574978a13a4c0333ca5deef4
Anthony Le <quangld@gearvn.com>
2020-08-06 22:53:30 +0700

    update variable

--- web-tinhocngoisao/wp-content/plugins/woocommerce-build-pc/private/reactjsBM/variable.js
+++ web-tinhocngoisao/wp-content/plugins/woocommerce-build-pc/private/reactjsBM/variable.js
@@ -1,5 +1,5 @@
 
 const localhost = 'http://localhost:9999/StarComputer/star_ecommerce/web-tinhocngoisao/';
-const serverhost = 'http://beta.tinhocngoisao.com/';
+const serverhost = 'http://traucay.com/';;
 export const HOST_URL = serverhost;
 export const HOST_URL_API = serverhost + 'wp-json/rest_api/v1/';
--- web-tinhocngoisao/wp-content/themes/online-shop-child/private/reactSrc/variable.js
+++ web-tinhocngoisao/wp-content/themes/online-shop-child/private/reactSrc/variable.js
@@ -1,5 +1,5 @@
 var HOST = 'http://localhost:9999/starComputer/star_ecommerce/web-tinhocngoisao/';
-const SERVER_HOST = 'https://tinhocngoisao.com/';
+const SERVER_HOST = 'http://traucay.com/';
 HOST = SERVER_HOST;
 export const URL_API_GET_LIST_PRODUCT_ON_SALE = HOST + 'wp-json/rest_api/v1/primetime_pricing?type=json&post_type=reactjs&response=json&cat_slug={cat_slug}&post_per_page={post_per_page}';
 export const HOST_URL = HOST;
