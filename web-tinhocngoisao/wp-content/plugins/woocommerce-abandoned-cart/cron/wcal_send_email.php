edc9f1da9293c524625f3bf6dcc6cc5646b0aa65
Anthony Le <quangld@gearvn.com>
2020-08-12 22:58:52 +0700

    update plugin Abandoned Cart Lite for WooCommerce

--- web-tinhocngoisao/wp-content/plugins/woocommerce-abandoned-cart/cron/wcal_send_email.php
+++ web-tinhocngoisao/wp-content/plugins/woocommerce-abandoned-cart/cron/wcal_send_email.php
@@ -68,9 +68,9 @@ if ( !class_exists( 'woocommerce_abandon_cart_cron' ) ) {
                 $day_seconds  = 86400; // 24 * 60 * 60
                 foreach ( $results as $key => $value ) {
                     if ( $value->day_or_hour == 'Days' ) {
-                        $time_to_send_template_after = $value->frequency * $day_seconds;
+                        $time_to_send_template_after = intval( $value->frequency ) * $day_seconds;
                     } elseif ( $value->day_or_hour == 'Hours' ) {
-                        $time_to_send_template_after = $value->frequency * $hour_seconds;
+                        $time_to_send_template_after = intval( $value->frequency ) * $hour_seconds;
                     }
 
                     $carts               = $this->wcal_get_carts( $time_to_send_template_after, $cart_abandon_cut_off_time, $value->id );
@@ -182,20 +182,20 @@ if ( !class_exists( 'woocommerce_abandon_cart_cron' ) ) {
                                         if( true == $wcal_check_cart_total ) {
                                             if ( $value->user_type == "GUEST" ) {
                                                 if ( isset( $results_guest[0]->billing_first_name ) ) {
-                                                    $email_body    = str_replace( "{{customer.firstname}}", $results_guest[0]->billing_first_name, $email_body );
-                                                    $email_subject = str_replace( "{{customer.firstname}}", $results_guest[0]->billing_first_name, $email_subject );
+                                                    $email_body    = str_ireplace( "{{customer.firstname}}", $results_guest[0]->billing_first_name, $email_body );
+                                                    $email_subject = str_ireplace( "{{customer.firstname}}", $results_guest[0]->billing_first_name, $email_subject );
                                                 }
                                                 if ( isset( $results_guest[0]->billing_last_name ) ) {
-                                                    $email_body = str_replace( "{{customer.lastname}}", $results_guest[0]->billing_last_name, $email_body );
+                                                    $email_body = str_ireplace( "{{customer.lastname}}", $results_guest[0]->billing_last_name, $email_body );
                                                 }
                                                 if ( isset( $results_guest[0]->billing_first_name ) && isset( $results_guest[0]->billing_last_name ) ) {
-                                                    $email_body = str_replace( "{{customer.fullname}}", $results_guest[0]->billing_first_name." ".$results_guest[0]->billing_last_name, $email_body );
+                                                    $email_body = str_ireplace( "{{customer.fullname}}", $results_guest[0]->billing_first_name." ".$results_guest[0]->billing_last_name, $email_body );
                                                 }
                                                 else if ( isset( $results_guest[0]->billing_first_name ) ) {
-                                                    $email_body = str_replace( "{{customer.fullname}}", $results_guest[0]->billing_first_name, $email_body );
+                                                    $email_body = str_ireplace( "{{customer.fullname}}", $results_guest[0]->billing_first_name, $email_body );
                                                 }
                                                 else if ( isset( $results_guest[0]->billing_last_name ) ) {
-                                                    $email_body = str_replace( "{{customer.fullname}}", $results_guest[0]->billing_last_name, $email_body );
+                                                    $email_body = str_ireplace( "{{customer.fullname}}", $results_guest[0]->billing_last_name, $email_body );
                                                 }
                                             } else {
                                                 $user_first_name      = '';
@@ -210,8 +210,8 @@ if ( !class_exists( 'woocommerce_abandon_cart_cron' ) ) {
                                                 } else {
                                                     $user_first_name = $user_first_name_temp;
                                                 }
-                                                $email_body          = str_replace( "{{customer.firstname}}", $user_first_name, $email_body );
-                                                $email_subject       = str_replace( "{{customer.firstname}}", $user_first_name, $email_subject );
+                                                $email_body          = str_ireplace( "{{customer.firstname}}", $user_first_name, $email_body );
+                                                $email_subject       = str_ireplace( "{{customer.firstname}}", $user_first_name, $email_subject );
                                                 $user_last_name      = '';
                                                 $user_last_name_temp = get_user_meta( $value->user_id, 'billing_last_name', true );
                                                 if( isset( $user_last_name_temp ) && "" == $user_last_name_temp ) {
@@ -224,8 +224,8 @@ if ( !class_exists( 'woocommerce_abandon_cart_cron' ) ) {
                                                 } else {
                                                     $user_last_name = $user_last_name_temp;
                                                 }
-                                                $email_body = str_replace( "{{customer.lastname}}", $user_last_name, $email_body );
-                                                $email_body = str_replace( "{{customer.fullname}}", $user_first_name." ".$user_last_name, $email_body );
+                                                $email_body = str_ireplace( "{{customer.lastname}}", $user_last_name, $email_body );
+                                                $email_body = str_ireplace( "{{customer.fullname}}", $user_first_name." ".$user_last_name, $email_body );
                                             }
                                             $order_date  = "";
                                             if( $cart_update_time != "" && $cart_update_time != 0 ) {
@@ -233,7 +233,7 @@ if ( !class_exists( 'woocommerce_abandon_cart_cron' ) ) {
                                                 $time_format = date_i18n( get_option( 'time_format' ), $cart_update_time );
                                                 $order_date = $date_format . ' ' . $time_format;
                                             }
-                                            $email_body = str_replace( "{{cart.abandoned_date}}", $order_date, $email_body );
+                                            $email_body = str_ireplace( "{{cart.abandoned_date}}", $order_date, $email_body );
                                             $query_sent = "INSERT INTO `".$wpdb->prefix."ac_sent_history_lite` ( template_id, abandoned_order_id, sent_time, sent_email_id )
                                                            VALUES ( %s, %s, '".current_time( 'mysql' )."', %s )";
 
@@ -262,7 +262,7 @@ if ( !class_exists( 'woocommerce_abandon_cart_cron' ) ) {
                                                 $encoding_cart   = $email_sent_id.'&url='.$cart_page_link;
                                                 $validate_cart   = $this->wcal_encrypt_validate( $encoding_cart );
                                                 $cart_link_track = get_option('siteurl').'/?wcal_action=track_links&validate=' . $validate_cart;
-                                                $email_body      = str_replace( "{{cart.link}}", $cart_link_track, $email_body );
+                                                $email_body      = str_ireplace( "{{cart.link}}", $cart_link_track, $email_body );
 
                                                 $validate_unsubscribe          = $this->wcal_encrypt_validate( $email_sent_id );
                                                 if ( count( $results_sent ) > 0 && isset( $results_sent[0]->sent_email_id ) ) {
@@ -271,28 +271,32 @@ if ( !class_exists( 'woocommerce_abandon_cart_cron' ) ) {
                                                 $encrypt_email_sent_id_address = hash( 'sha256', $email_sent_id_address );
                                                 $plugins_url                   = get_option( 'siteurl' ) . "/?wcal_track_unsubscribe=wcal_unsubscribe&validate=" . $validate_unsubscribe . "&track_email_id=" . $encrypt_email_sent_id_address;
                                                 $unsubscribe_link_track        = $plugins_url;
-                                                $email_body                    = str_replace( "{{cart.unsubscribe}}" , $unsubscribe_link_track , $email_body );
+                                                $email_body                    = str_ireplace( "{{cart.unsubscribe}}" , $unsubscribe_link_track , $email_body );
                                                 $var = '';
                                                 if( preg_match( "{{products.cart}}", $email_body, $matched ) ) {
+                                                    $qty_header           = __( "Quantity", "woocommerce-abandoned-cart" );
+                                                    $price_header         = __( "Price", "woocommerce-abandoned-cart" );
+                                                    $line_subtotal_header = __( "Line Subtotal", "woocommerce-abandoned-cart" );
+                                                    
                                                     if ( class_exists( 'WP_Better_Emails' ) ) {
-                                                        $var = '<table width = 100%>
+                                                        $var = '<table width = 100% style="margin-right: auto; margin-left:auto;">
                                                                 <tr> <td colspan="5"> <h3>'.__( "Your Shopping Cart", "woocommerce-abandoned-cart" ).'</h3> </td></tr>
                                                                 <tr>
                                                                 <th>'.__( "Item", "woocommerce-abandoned-cart" ).'</th>
                                                                 <th>'.__( "Name", "woocommerce-abandoned-cart" ).'</th>
-                                                                <th>'.__( "Quantity", "woocommerce-abandoned-cart" ).'</th>
-                                                                <th>'.__( "Price", "woocommerce-abandoned-cart" ).'</th>
-                                                                <th>'.__( "Line Subtotal", "woocommerce-abandoned-cart" ).'</th>
+                                                                <th>' . apply_filters( 'wcal_reminder_email_qty_header', $qty_header ) . '</th>
+                                                                <th>' . apply_filters( 'wcal_reminder_email_price_header', $price_header ) . '</th>
+                                                                <th>' . apply_filters( 'wcal_reminder_email_line_subtotal_header', $line_subtotal_header ) . '</th>
                                                                 </tr>';
                                                     } else {
-                                                        $var = '<h3>'.__( "Your Shopping Cart", "woocommerce-abandoned-cart" ).'</h3>
-                                                            <table border="0" cellpadding="10" cellspacing="0" class="templateDataTable">
+                                                        $var = '<table border="0" cellpadding="10" cellspacing="0" class="templateDataTable" style="margin-right: auto; margin-left:auto;">
+                                                        <tr> <td colspan="5"> <h3>'.__( "Your Shopping Cart", "woocommerce-abandoned-cart" ).'</h3> </td></tr>
                                                                 <tr>
                                                                 <th>'.__( "Item", 'woocommerce-abandoned-cart' ).'</th>
                                                                 <th>'.__( "Name", 'woocommerce-abandoned-cart' ).'</th>
-                                                                <th>'.__( "Quantity", 'woocommerce-abandoned-cart' ).'</th>
-                                                                <th>'.__( "Price", 'woocommerce-abandoned-cart' ).'</th>
-                                                                <th>'.__( "Line Subtotal", 'woocommerce-abandoned-cart' ).'</th>
+                                                                <th>' . apply_filters( 'wcal_reminder_email_qty_header', $qty_header ) . '</th>
+                                                                <th>' . apply_filters( 'wcal_reminder_email_price_header', $price_header ) . '</th>
+                                                                <th>' . apply_filters( 'wcal_reminder_email_line_subtotal_header', $line_subtotal_header ) . '</th>
                                                                 </tr>';
                                                     }
                                                     $cart_details = $cart_info_db_field->cart;
@@ -358,12 +362,13 @@ if ( !class_exists( 'woocommerce_abandon_cart_cron' ) ) {
                                                                 }
                                                                 $product_name = $product_name_with_variable;
                                                             }
+                                                            $product_name = apply_filters( 'wcal_reminder_email_after_product_name', $product_name, $v );
                                                             $var .='<tr align="center">
                                                                     <td> <a href="'.$cart_link_track.'"> <img src="' . $image_url . '" alt="" height="42" width="42" /> </a></td>
                                                                     <td> <a href="'.$cart_link_track.'">'.$product_name.'</a></td>
-                                                                    <td> '.$quantity_total.'</td>
-                                                                    <td> '.$item_subtotal.'</td>
-                                                                    <td> '.$item_total_display.'</td>
+                                                                    <td> ' . apply_filters( 'wcal_reminder_email_qty_value', $quantity_total ) . '</td>
+                                                                    <td> ' . apply_filters( 'wcal_reminder_email_price_value', $item_subtotal ) . '</td>
+                                                                    <td> ' . apply_filters( 'wcal_reminder_email_line_subtotal_value', $item_total_display ) . '</td>
                                                                 </tr>';
                                                             $cart_total += $item_total;
                                                             $item_subtotal = $item_total = 0;
@@ -378,20 +383,21 @@ if ( !class_exists( 'woocommerce_abandon_cart_cron' ) ) {
 
                                                     if ( $p_exists ) {
                                                         $cart_total = wc_price( $cart_total );
+                                                        $cart_total = apply_filters( 'wcal_reminder_email_cart_total', $cart_total );
+                                                        $cart_total_title = __( "Cart Total:", 'woocommerce-abandoned-cart' );
                                                         $var .= '<tr align="center">
                                                                 <td> </td>
                                                                 <td> </td>
                                                                 <td> </td>
-                                                                <td>'.__( "Cart Total:", 'woocommerce-abandoned-cart' ).'</td>
+                                                                <td>' . apply_filters( 'wcal_reminder_email_cart_total_title', $cart_total_title ) . '</td>
                                                                 <td> '.$cart_total.'</td>
                                                             </tr>';
-                                                        $var .= '</table>
-                                                                                ';
-                                                        $email_body    = str_replace( "{{products.cart}}", $var, $email_body );
-                                                        $email_subject = str_replace( "{{product.name}}", $sub_line_prod_name, $email_subject );
+                                                        $var .= '</table>';
+                                                        $email_body    = str_ireplace( "{{products.cart}}", $var, $email_body );
+                                                        $email_subject = str_ireplace( "{{product.name}}", $sub_line_prod_name, $email_subject );
                                                     }else {
-                                                        $email_body    = str_replace( "{{products.cart}}", __( 'Product no longer exists', 'woocommerce-abandoned-cart' ), $email_body );
-                                                        $email_subject = str_replace( "{{product.name}}", $sub_line_prod_name, $email_subject );
+                                                        $email_body    = str_ireplace( "{{products.cart}}", __( 'Product no longer exists', 'woocommerce-abandoned-cart' ), $email_body );
+                                                        $email_subject = str_ireplace( "{{product.name}}", $sub_line_prod_name, $email_subject );
                                                     }
                                                 }
 
@@ -410,7 +416,7 @@ if ( !class_exists( 'woocommerce_abandon_cart_cron' ) ) {
                                                         $email_body_template_footer = ob_get_clean();
 
                                                         $site_title = get_bloginfo( 'name' );
-                                                        $email_body_template_footer     = str_replace( '{site_title}', $site_title, $email_body_template_footer );
+                                                        $email_body_template_footer     = str_ireplace( '{site_title}', $site_title, $email_body_template_footer );
 
                                                         $final_email_body =  $email_body_template_header . $email_body_final . $email_body_template_footer;
 
