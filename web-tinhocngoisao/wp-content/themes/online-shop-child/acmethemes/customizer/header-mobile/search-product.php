fcf7a3f279e32b621b96fa032668da3ea3d49174
Anthony Le <quangld@gearvn.com>
2021-08-21 11:05:18 +0700

    Xóa old theme

--- web-tinhocngoisao/wp-content/themes/online-shop-child/acmethemes/customizer/header-mobile/search-product.php
+++
@@ -1,3 +0,0 @@
-<div class="advance-product-search">
-    <?php if ( function_exists( 'aws_get_search_form' ) ) { aws_get_search_form(); } ?>
-</div><!-- .advance-product-search -->
--- web-tinhocngoisao/wp-content/themes/online-shop-child/templates/checkout/checkout-head.php
+++
@@ -1,9 +0,0 @@
-
-<?php 
-    function renderMetaViewport() {
-?>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<?php    }
-    add_action('wp_head', 'renderMetaViewport');
-    wp_head();
-?>
